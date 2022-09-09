1d0279c6f6e7fdf15a4bb84d4aefcac80678b9a4
ruslanzverkov <createmed@mail.ru>
2022-09-09 21:35:56 +0300

    12

---
+++ src/Bicycle.java
@@ -0,0 +1,12 @@
+public class Bicycle extends Car {
+
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void updateTyre() {
+            System.out.println("Меняем покрышку");
+        }
+
+}
